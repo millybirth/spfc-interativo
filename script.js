5dc31a1fa3c803e7876f93cdfbff81da99f83bb9
millybirth <57635217+millybirth@users.noreply.github.com>
2025-06-20 11:02:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function animarEscudo() {
-  const escudo = document.querySelector('.escudo');
+  const escudo = document.querySelector('.escudo.real');
   const mensagem = document.getElementById('mensagem');
   escudo.classList.add('ativo');
   setTimeout(() => escudo.classList.remove('ativo'), 600);
@@ -7,10 +7,10 @@ function animarEscudo() {
 }
 
 for (let i = 0; i < 15; i++) {
-  const coração = document.createElement("div");
-  coração.className = "coração";
-  coração.innerText = "❤️";
-  coração.style.left = Math.random() * 100 + "vw";
-  coração.style.animationDuration = 4 + Math.random() * 4 + "s";
-  document.body.appendChild(coração);
+  const c = document.createElement("div");
+  c.className = "coração";
+  c.innerText = "❤️";
+  c.style.left = Math.random() * 100 + "vw";
+  c.style.animationDuration = 4 + Math.random() * 4 + "s";
+  document.body.appendChild(c);
 }
